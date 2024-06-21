d705365c696ce45b363181f2aa9877872abc754a
Stefano Mini <info@stefanominiconsulting.com>
2024-06-21 11:56:48 +0300

    credenziali emailhandler

--- py/post_hander/email_handler.py
+++ py/post_hander/email_handler.py
@@ -1,7 +1,23 @@
 from py.shared.logger import get_logger
+import smtplib
+from email.message import EmailMessage
+from py.shared.data_reader import DataReader
 
 
 class EmailHandler:
-    def __init__(self, emails: list[str]):
+    def __init__(self, options: str):
         self.logger = get_logger(__name__)
-        self.emails = emails
+        self.options = options
+        self.secrets = DataReader("data_secret.json").read_json()
+        self.emails: list[str] = self.secrets["emails"]
+        self.credentials = self.secrets["ses_credentials"]
+        self.server = self.credentials["smtp_endpoint"]
+        self.tls_port = self.credentials["tls_wrapper_port"]
+        self.user = self.credentials["smtp_user"]
+        self.password = self.credentials["smtp_password"]
+        self.sender = self.credentials["sender_email"]
+        self.subject = self.credentials["email_subject"]
+        self.body_template = self.credentials["email_body"]
+        self.body = self.body_template.format(opzioni=options)
+    
+    def send_emails(self) -> None:
--- py/post_hander/post_hander.py
+++ py/post_hander/post_hander.py
@@ -1,16 +1,18 @@
 from py.shared.logger import get_logger
 from py.post_hander.email_handler import EmailHandler
 from py.post_hander.alert_handler import AlertHandler
+from py.shared.data_reader import DataReader
 
 
 class NieHander:
     def __init__(self):
         self.logger = get_logger(__name__)
+        self.secrets = DataReader("data_secret.json").read_json()
 
     def handle_nie(self, options: list[str]) -> None:
         if options:
             alert_hander = AlertHandler()
             alert_hander.show_notification("Opzioni NIE trovate!", f"Opzioni disponibili: {" ||| ".join(options)}")
-            email_hander = EmailHandler("test@test.com")
+            email_hander = EmailHandler(" ||| ".join(options))
         else:
             self.logger.info("No NIE options found.")
