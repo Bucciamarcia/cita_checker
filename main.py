b0165a48f3092812a3b199641211848515efdb6c
Stefano Mini <info@stefanominiconsulting.com>
2024-06-21 10:40:51 +0300

    Add NieHander to handle NIE options in main.py

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from py.selenium.selenium import Selenium
+from py.post_hander.post_hander import NieHander
+
+
+def nie():
+    scraper = Selenium()
+    nie_options = scraper.check_nie
+    NieHander().handle_nie(nie_options)
 
 
 def main():
-    scraper = Selenium()
-    nie_options = scraper.check_nie()
+    nie()
 
 
 if __name__ == "__main__":
